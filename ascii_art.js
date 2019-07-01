11217f025707b604c158bdaa92b8d985696ff011
Chad Jemmett <chad.jemmett@gmail.com>
2019-07-01 17:29:12 -0600

    Addes ascii problem

---
+++ ascii_art.js
@@ -0,0 +1,8 @@
+return a string of ascii characters.
+
+
+const asciiRenderer = (max_x, max_y, pixels) => {
+  // pixels is an array of stringified arrays.
+    // They look like this "[0, 0, 99]" where the first one is the x coordinate. The second one is the y coordinate. The third is the number of the ascii character.
+
+}
