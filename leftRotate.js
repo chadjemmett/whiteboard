cb38f8a8bcc67692f12f300dc035537fae8a87f3
Chad Jemmett <chad.jemmett@gmail.com>
2019-07-26 09:32:45 -0600

    Rotate the array thing

---
+++ leftRotate.js
@@ -0,0 +1,25 @@
+ const a = [ 1, 2, 3, 4, 5 ]
+const leftRotate = (arr, d) => {
+  const newArray = new Array(arr.length).fill(null)
+    for(let i = 0; i<arr.length;i++) {
+      if(i - d < 0) {
+        console.log(i - d)
+        newArray[arr.length - Math.abs(i - d)] = arr[i]
+      } else {
+        newArray[i - d] = arr[i]
+      }
+    }
+
+ return newArray
+}
+
+console.log(leftRotate(a, 4))
+console.log(leftRotate(a, 2))
+
+
+
+  // 1. create a new array the length of the incoming array.
+  // 2. loop through each number in the passed in array. Subtract the number of rotattions. to find the new spot in the array.
+  // 3. If the number is less than zero. Go to the absolute value of those.
+
+
