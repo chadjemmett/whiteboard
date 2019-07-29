b9dfc884ae2df148d29d84ad5fd6d01b09a04d69
Chad Jemmett <chad.jemmett@gmail.com>
2019-07-29 16:27:34 -0600

    Working on kangaroo

--- appleAndOrange.js
+++ appleAndOrange.js
@@ -1,5 +1,35 @@
 
+//s integers startin point of sams house
+//t ending point of sams house
+//a location of apple tree
+//b location of orange tree.
+const applesOranges = (s, t, a, b, apples, oranges) => {
+  let applesLanding = 0
+  let orangesLanding = 0
+ // Apples
+ //   if the number is greater than A plus the fruit number, then check to see if it is greater than or equal to S.
+ //     If it is increment the apple number.
+ // Oranges.
+ //  if the number is less than or B plus fruit number, then check to see if it's less than or equal to T.
+ //    If it is increment the orange number
+
+  for (let i = 0 ; i< apples.length; i++) {
+    if(apples[i] + a >= s ) {
+      applesLanding += 1
+    }
+  }
+  
+  for (let i = 0 ; i< oranges.length; i++) {
+    if(oranges[i] + b <= t ) {
+      orangesLanding += 1
+    }
+  }
+
+  console.log("apples", applesLanding)
+  console.log("oranges", orangesLanding)
+
+}
 
 
 
-
+console.log(applesOranges(7, 11, 5, 15, [-2, 2, 1], [5, -6]))
--- time_conversion.js
+++ time_conversion.js
@@ -5,15 +5,41 @@ const midnight = "12:05:20AM"
 function timeConvert(s) {
   // const newString = s.substring(2, s.length - 2)
   let newString;
-  let hour = parseInt(s.substring(0, 2))
+  let hour = s.substring(0, 2)
   const ampm = s.substring(s.length - 2, s.length)
 
+  if(ampm === "AM") {
+    if(hour === 12) {
+      return "00" + s.substring(2, s.length - 2)
+    } else {
+      return hour + s.substring(2, s.length - 2)
+    }
+  } else {
+    if(hour === 12) {
+      return hour + s.substring(2, s.length - 2)
+    } else {
+      return hour + s.substring(2, s.length - 2)
+
+    }
+
+  }
+
+  // if (hour === 12 && ampm === "AM") {
+  //         return "00" + s.substring(2, s.length - 2)
+
+  //     } else if (hour === 12 && ampm === "PM") {
+  //         return hour + s.substring(2, s.length - 2)
+  //     } else {
+  //         hour = (hour + 12).toString()
+  //         return hour + s.substring(2, s.length - 2)
+  //     }
 
 
-
-}
+  }
 
 
 console.log(timeConvert(time))
 console.log(timeConvert(noon))
 console.log(timeConvert(midnight))
+console.log(timeConvert("06:40:03AM"))
+console.log(timeConvert("11:40:03AM"))
