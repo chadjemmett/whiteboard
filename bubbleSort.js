22ebc253f5ce2a8c3e5bcdea239f12d7763b6a9d
Chad Jemmett <chad.jemmett@gmail.com>
2019-07-29 09:11:38 -0600

    Added some files

---
+++ bubbleSort.js
@@ -0,0 +1,37 @@
+const a = [4, 7, 1, 0, 99, 20, 100]
+
+
+const bubbleSort = (arr) => {
+  // let sorting = true 
+  // let pointer = 0
+
+  // while(sorting) {
+  //   if(pointer === arr.length - 1) {
+  //     pointer = 0
+  //     sorting = false
+  //   } else {
+  //     if(arr[pointer] > arr[pointer + 1]) {
+  //       arr[pointer], arr[pointer + 1] = arr[pointer+1], arr[pointer]
+  //       console.log("ponter is bigger so swap", arr[pointer], arr[pointer + 1])
+  //     }
+  //     pointer += 1
+  //   }
+  // }
+
+
+
+    return arr
+  }
+
+
+
+
+
+// 1. set a sortring var to true. set a pointer to 0
+// 2. go through the while loop  as long as sorting is true
+// 3. take arr[0] and arr[1].
+// 4. If arr[0] is larger. Swap places. Set sorting to true
+// check to see if you are at the end. Check to see if pointer is at the length of the array minus 1
+// if we are at the end and sorting is false. Then break the loop. return the array.
+// 5. Go to arr[1] and get arr[2] 
+ 
