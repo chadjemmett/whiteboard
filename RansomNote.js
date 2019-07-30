69da44dd8a991a3f0388f2e505ba34b275f0fc9d
Chad Jemmett <chad.jemmett@gmail.com>
2019-07-30 14:00:58 -0600

    Added array DS and did kangroo prblem

--- RansomNote.js
+++ RansomNote.js
@@ -6,20 +6,33 @@ const checkMagazine = (magazine, note) => {
     // check each one against the name in the magazine array O(n^2)
     // If the word appears in the magazine, check for lowercase/uppercase
     //o
-    //
-    let canMatchWords = null
-      for(let i = 0; i<note.length; i++) {
-        for(let k = 0; k<magazine.note; k++) {
-
-
+      let counter = 0
+      const words = []
+      for(let i = 0; i< note.length; i++) {
+        for(let k = 0; k< magazine.length; k++) {
+          if(note[i] === magazine[k]) {
+            console.log("mag words", magazine[k])
+            words.push(magazine[k])
+          }
         }
-
-
       }
+      console.log(words)
 
+    if( note.sort() === words.sort()) {
+      return "YES"
+    } else {
+      return "NO"
+    }
 
 }
 
 
 
 console.log(checkMagazine(m, n))
+
+
+  // loop through both arrays.
+  // Check each note against the magazine words.
+  // Keep looping if 
+  //
+  console.log("Apple" === "apple")
--- kangaroo.js
+++ kangaroo.js
@@ -2,5 +2,34 @@
 
 
 const kangaroos = (x1, v1, x2, v2) =>  {
+  // take the one thats first.
+  // take a jump. add it to the array.
+  // the second one takes a jump.
+  // check that number 
+  let firstKangaroo;
 
-}
+  if(x1 > x2) {
+    firstKangaroo = x1
+    secondKangaroo = x2
+  } else {
+    firstKangaroo = x2
+    secondKangaroo = x1
+  } 
+  while (firstKangaroo < 10000) {
+    firstKangaroo += 
+
+
+
+  }
+
+
+
+
+
+// kangaroo 1. starts at 1 jumps 1 meter a jump
+// kangaroo 2 starts at 2. Jumps 2 meters a jump
+// When do they land on the same spotl.
+
+
+kangaroos(0, 3, 4, 2)
+
