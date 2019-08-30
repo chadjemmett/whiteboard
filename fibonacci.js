ab6114dddbaf657e05d90fb2ac7da06c548dadd2
Chad Jemmett <chad.jemmett@gmail.com>
2019-08-30 17:42:49 -0600

    Aug 30

---
+++ fibonacci.js
@@ -0,0 +1,22 @@
+
+
+// const fibonacci = (n) => {
+//   let a = 1, b = 0, temp;
+//   while (n >= 0) {
+//     temp = a;
+//     a = a + b;
+//     b = temp;
+//   console.log(b)
+//     n --
+//   }
+
+//   return b
+
+// }
+
+
+// console.log(fibonacci(8))
+//
+//
+//
+
