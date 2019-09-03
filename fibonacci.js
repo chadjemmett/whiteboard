ec5304bf2b72a2ab281f4851efc8b6aae646b5a4
Chad Jemmett <chad.jemmett@gmail.com>
2019-09-03 15:22:10 -0600

    Fibonacci numbers with recursion and caching

--- fibonacci.js
+++ fibonacci.js
@@ -1,22 +1,51 @@
 
 
-// const fibonacci = (n) => {
-//   let a = 1, b = 0, temp;
-//   while (n >= 0) {
-//     temp = a;
-//     a = a + b;
-//     b = temp;
-//   console.log(b)
-//     n --
-//   }
-
-//   return b
-
-// }
 
 
-// console.log(fibonacci(8))
-//
-//
-//
+const fibonacci = ( number ) => {
+  let a = 1
+  let b = 0
+  let temp = 0
+  while(number >= 0) {
+    temp = a
+    a = a + b
+    b = temp
+    // console.log(b, "is fibonacci number: ", number)
+    number --
+  }
 
+  return b
+
+}
+
+
+
+const fibRecurse = (number) => {
+  console.log(number)
+  if(number <= 1){
+    return 1
+  }
+
+  return fibRecurse(number - 1) + fibRecurse(number - 2)
+
+
+}
+
+
+const fibMemo = (number, m) => {
+  memo = m  || {}
+  // console.log(memo)
+  if(memo[number]) {
+    return memo[number]
+  }
+
+  if(number <= 1) {
+    return 1
+  }
+
+  return memo[number] = fibMemo(number - 1, memo) + fibMemo(number - 2, memo)
+
+}
+// console.log(fibonacci(50))
+// console.log(fibRecurse(10))
+console.log(fibMemo(10))
