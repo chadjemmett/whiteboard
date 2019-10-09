c2ed791e15395bcde278b1b20ebfb284b81049fd
Chad Jemmett <chad.jemmett@gmail.com>
2019-10-09 10:40:47 -0600

    Working on merge sort

--- fibonacci.js
+++ fibonacci.js
@@ -4,48 +4,51 @@
 
 const fibonacci = ( number ) => {
   let a = 1
-  let b = 0
+  let fib = 0
   let temp = 0
+  const array = []
+
   while(number >= 0) {
     temp = a
-    a = a + b
-    b = temp
-    // console.log(b, "is fibonacci number: ", number)
+    a = a + fib 
+    fib = temp
+    console.log("temp", temp, "a", a, "fibb", fib)
     number --
   }
 
-  return b
+  return fib
 
 }
+console.log(fibonacci(5))
 
 
 
-const fibRecurse = (number) => {
-  console.log(number)
-  if(number <= 1){
-    return 1
-  }
+// const fibRecurse = (number) => {
+//   console.log(number)
+//   if(number <= 1){
+//     return 1
+//   }
 
-  return fibRecurse(number - 1) + fibRecurse(number - 2)
-
-
-}
+//   return fibRecurse(number - 1) + fibRecurse(number - 2)
 
 
-const fibMemo = (number, m) => {
-  memo = m  || {}
-  // console.log(memo)
-  if(memo[number]) {
-    return memo[number]
-  }
+// }
 
-  if(number <= 1) {
-    return 1
-  }
 
-  return memo[number] = fibMemo(number - 1, memo) + fibMemo(number - 2, memo)
+// const fibMemo = (number, m) => {
+//   memo = m  || {}
+//   // console.log(memo)
+//   if(memo[number]) {
+//     return memo[number]
+//   }
 
-}
-// console.log(fibonacci(50))
-// console.log(fibRecurse(10))
-console.log(fibMemo(10))
+//   if(number <= 1) {
+//     return 1
+//   }
+
+//   return memo[number] = fibMemo(number - 1, memo) + fibMemo(number - 2, memo)
+
+// }
+// // console.log(fibonacci(50))
+// // console.log(fibRecurse(10))
+// console.log(fibMemo(10))
--- mergesort.js
+++ mergesort.js
@@ -12,6 +12,31 @@ const array = [ 3, 5, 1, 9, 4  ]
 
 
 // ??
+const mergeArrays = (left, right) => {
+  return [left, right]
+}
+
+const splitArray = (arr) => {
+  // check to see if array is length of 1
+  if(arr.length === 1) {
+    // if it is, return array
+    return arr
+  }
+
+  // find the middle of the array
+  const middle = Math.round(arr.length / 2)
+  // const middle = arr[Math.floor(arr.length / 2)]
+  const leftSide = arr.slice(0, middle)
+  const rightSide = arr.slice(middle, arr.length)
+  leftSorted = splitArray(leftSide)
+  rightSorted = splitArray(rightSide)
+
+  // console.log("left", leftSide, "right", rightSide)
+
+  return mergeArrays(leftSorted, rightSorted)
+
+
+}
 
 
 // [[3], [5], [1], [9], [4]]
@@ -22,46 +47,18 @@ const array = [ 3, 5, 1, 9, 4  ]
 // [3][5][1][9][4]
 //
 
-const splitSet = (arr) => {
-  if(arr.length === 1) {
-    return arr.flat(3)
-  } else {
-    // find the middle
-    let middle = Math.floor(arr.length / 2)  //<= 2
-    // arr[0] to middle
-    // call the method on the first half.
-    // Then call the metho on the second half. middel to arr.length -1
-    //
-      // pop into a new array.
-      // starting at 0 then going to the middle.
-      // then do that again for second only starting adt the middle and going to the end.
-      let firstHalf = arr.slice(0, middle) 
-      let secondHalf = arr.slice(middle) 
-        // for (let i = 0; i< middle; i++) {
-        //   firstHalf.push(arr[i])
-        // }
-
-        // for (let i = middle; i < arr.length ; i++) {
-        //   secondHalf.push(arr[i])
-        // }
-   return splitSet(firstHalf)
-   // return mergeSort(splitSet(firstHalf), splitSet(secondHalf))
-
-  }
-
-}
 
 
-compare the left and right.
-check to see if the left and right are the same.
-if the last element in the left array is larger than the first element in the right array. Merge them. Merge right + left 
-if the last element in the left array is smaller than the first element in the right array. Merge them Merge them left + right.
-because those two arrays will already be sorted.
-do this until there
+// compare the left and right.
+// check to see if the left and right are the same.
+// if the last element in the left array is larger than the first element in the right array. Merge them. Merge right + left 
+// if the last element in the left array is smaller than the first element in the right array. Merge them Merge them left + right.
+// because those two arrays will already be sorted.
+// do this until there
 
-const splitArray = splitSet(array)
-const newOne = []
+// const splitArray = splitSet(array)
+// const newOne = []
 
-console.log(splitArray)
+console.log(splitArray(array))
 
 
