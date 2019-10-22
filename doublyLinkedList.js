02d8f08f3a227b53745ebf845d3c6811a2798cb1
Chad Jemmett <chad.jemmett@gmail.com>
2019-10-22 15:38:28 -0600

    Added doubly linked list

---
+++ doublyLinkedList.js
@@ -0,0 +1,22 @@
+
+class Node {
+  constructor(value) {
+    this.value = value
+    this.next = null
+    this.prev = null
+  }
+}
+
+class DLL {
+  constructor() {
+    this.length = 0
+  }
+
+  addNode =() => {
+
+  }
+
+
+
+
+}
